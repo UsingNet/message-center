03cd1c7531b295cf971296c445f52878c3261ec3
easthing <easthing@gmail.com>
2016-11-08 16:40:28 +0800

    fix config

--- config.js
+++ config.js
@@ -4,8 +4,9 @@ module.exports = {
     host: '127.0.0.1',
     port: 6379,
   },
-  mongodb: process.env.NODE_ENV === 'production' ?
-    'mongodb://h1.mongo.nosql/message_center' : 'mongodb://127.0.0.1/message_center',
+  mongodb: process.env.NODE_ENV === 'development' ?
+    'mongodb://127.0.0.1/message_center' :
+    'mongodb://h1.mongo.nosql/message_center',
   pusher: 'tcp://127.0.0.1:20002',
   logDir: `${__dirname}/logs`,
 };
